e4a14907b8629854a609ad333edb9f9df368fde7
AaronWang <aaronwang81@gmail.com>
2018-03-14 23:15:21 +0800

    no message

---
+++ smart_fun/smart_fun.ino
@@ -0,0 +1,63 @@
+#define controlPin 5
+#define out1 7
+#define out2 8
+
+void setPwmFrequency(int pin, int divisor) {
+  byte mode;
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 64: mode = 0x03; break;
+      case 256: mode = 0x04; break;
+      case 1024: mode = 0x05; break;
+      default: return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    } else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  } else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 32: mode = 0x03; break;
+      case 64: mode = 0x04; break;
+      case 128: mode = 0x05; break;
+      case 256: mode = 0x06; break;
+      case 1024: mode = 0x07; break;
+      default: return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+Serial.begin (9600);
+pinMode(controlPin, OUTPUT);
+pinMode(out1, OUTPUT);
+pinMode(out2, OUTPUT);
+setPwmFrequency(controlPin, 1);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(out1, HIGH);
+  //digitalWrite(out2, LOW);
+  analogWrite(controlPin, 20);
+
+//  for(int rate = 1; rate <= 10; ++rate)
+//  {
+//    analogWrite(controlPin, rate * 25.5);
+//  //Serial.write("192");
+//  delay((10 - rate - 1) * 1000);
+//  }
+
+//digitalWrite(out1, LOW);
+//digitalWrite(out2, LOW);
+
+}
