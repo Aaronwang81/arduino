624acc9edeb12fae5d7563cd4d3d3517dc49d856
AaronWang <aaronwang81@gmail.com>
2018-03-15 23:40:12 +0800

    增加3次测量求平均值。但没方差，测量速度很快，也许不需要平均值。

--- ultrasound/ultrasound.ino
+++ ultrasound/ultrasound.ino
@@ -4,27 +4,20 @@
 
 float distance;
 float temp;
+float dis1;
+float dis2;
+float dis3;
 
-
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(Trig, OUTPUT);
-  pinMode(Echo, INPUT);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-//给Trig发送一个低高低的短时间脉冲,触发测距  
-  digitalWrite(Trig, LOW); //给Trig发送一个低电平  
+float getDistance(int trigPin, int echoPin)
+{
+  //给Trig发送一个低高低的短时间脉冲,触发测距  
+  digitalWrite(trigPin, LOW); //给Trig发送一个低电平  
   delayMicroseconds(2);    //等待 2微妙  
-  digitalWrite(Trig,HIGH); //给Trig发送一个高电平  
+  digitalWrite(trigPin,HIGH); //给Trig发送一个高电平  
   delayMicroseconds(10);    //等待 10微妙  
-  digitalWrite(Trig, LOW); //给Trig发送一个低电平  
+  digitalWrite(trigPin, LOW); //给Trig发送一个低电平  
     
-  temp = float(pulseIn(Echo, HIGH)); //存储回波等待时间,  
+  temp = float(pulseIn(echoPin, HIGH)); //存储回波等待时间,  
   //pulseIn函数会等待引脚变为HIGH,开始计算时间,再等待变为LOW并停止计时  
   //返回脉冲的长度  
     
@@ -39,6 +32,29 @@ void loop() {
   Serial.print(temp);//串口输出等待时间的原始数据  
   Serial.print(" | | Distance = ");  
   Serial.print(distance);//串口输出距离换算成cm的结果  
-  Serial.println(" cm");  
+  Serial.println(" cm"); 
+  return distance;
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(Trig, OUTPUT);
+  pinMode(Echo, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  dis1 = getDistance(Trig, Echo);
+  dis2 = getDistance(Trig, Echo);
+  dis3 = getDistance(Trig, Echo);
+
+  distance = (dis1 + dis2 + dis3) / 3;
+
+  Serial.print("Average Distance = ");  
+  Serial.print(distance);//串口输出距离换算成cm的结果  
+  Serial.println(" cm"); 
+ 
   delay(100); 
 }
