6927d926a74873ce65ad1250af03bd21662be692
AaronWang <aaronwang81@gmail.com>
2018-03-21 17:41:10 +0800

    结构优化

--- TuringQA/src/main/java/com/turingqa/AppEntry.java
+++ TuringQA/src/main/java/com/turingqa/AppEntry.java
@@ -11,8 +11,6 @@ public class AppEntry {
         Request request = Request.makeRequest();
         Gson gson = new Gson();
 
-
-        request.perception.inputText.text = "父亲亲节时间";
         request.perception.inputImage.url = "http://test.com";
         request.perception.inputMedia.url = "http://media";
         request.perception.selfInfo.location.city = "广州";
@@ -21,11 +19,18 @@ public class AppEntry {
         request.userInfo.apiKey = "51a8fac0ecc3470fa9237cfd71a7b6ed";
         request.userInfo.userId = "123456";
 
-        String temp = gson.toJson(request);
-        System.out.println(temp);
-        String result = HttpUtils.sendPostMessage("http://openapi.tuling123.com/openapi/api/v2", temp,  "UTF-8");
-        Response response = gson.fromJson(result, Response.class);
-        System.out.println(result);
+        while(true){
+            request.perception.inputText.text = JOptionPane.showInputDialog("输入文字：");
+            if(null == request.perception.inputText.text || request.perception.inputText.text.isEmpty()){
+                break;
+            }
+            String temp = gson.toJson(request);
+            System.out.println(temp);
+            String result = HttpUtils.sendPostMessage("http://openapi.tuling123.com/openapi/api/v2", temp,  "UTF-8");
+            Response response = gson.fromJson(result, Response.class);
+            System.out.println(result);
+        }
+
         //System.out.println(response.intent.parameters.toString());
     }
 }
